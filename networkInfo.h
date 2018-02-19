3bdf585451c82147bbefc08dc3bacdada4ad492f
Jeevan Anga <jeevan.900929@gmail.com>
2018-02-19 23:03:26 +0800

    Remove nc file

--- networkInfo.h
+++
@@ -1,7 +0,0 @@
-#ifndef networkInfo.h
-#define networkInfo.h
-
-#define NETWORK_SSID "sepuluhTimahSari@unifi W8901G" //Set your network's SSID here
-#define NETWORK_PASSWORD "Abcd1234" //Set your network's password here
-
-#endif // networkInfo.h
