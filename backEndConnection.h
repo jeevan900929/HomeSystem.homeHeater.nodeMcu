1e77746ed1e42df535410668eeff0647f3f6a4fd
jeevan900929 <jeevan900929@debian-alienwareM14xR2.JEEVAN_HOME>
2018-03-03 11:37:48 +0800

    Update REST API from PUT to GET

--- backEndConnection.h
+++ backEndConnection.h
@@ -1,6 +1,7 @@
 #ifndef backEndConnection.h
 #define backEndConnection.h
 
+
 void reportToBackendServer(int event)
 {
   const char http_site[] = "www.example.com";
@@ -42,34 +43,51 @@ void updateServerState(int serverStateMode) //reportToBackendServer
   Serial.print("[HTTP] begin...\n");
   // configure traged server and url
   //http.begin("https://192.168.1.12/test.html", "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
-  String url0 = "http://192.168.1.11:3100/heaters/rear/5a827c6c5eafd22b646968d0";
-  http.begin(url0); //HTTP
+  String url0 = "http://192.168.1.11:3100/heaters/rear/5a97848ea83c1337a010c4d5";
+  String url1 = "http://192.168.1.11:3100/heaters/rear/heater9/false";
+  String url2 = "http://192.168.1.11:3100/heaters/rear/heater9/true";
+
+  //http.begin(url0); //HTTP
+
+  if (rearstatus)
+    http.begin(url1);
+  else
+    http.begin(url2);
+
 
   Serial.print("[HTTP] GET...\n");
   // start connection and send HTTP header
-  //int httpCode = http.GET();
-  int httpCode0;
+  int httpCode = http.GET();
 
-  http.addHeader("content-type", "application/json", false, false);
-  http.addHeader("authorization", "basic YWRtaW5pc3RyYXRvcjpwYXNzd29yZA==", false, false);
-  
-  String payload0;
+  /*
 
-  if (rearstatus)
-  {
-    payload0 = "{\"state\": \"true\"}\r\n";
-  }
-  else
-  {
-    payload0 = "{\"state\": \"false\"}\r\n";
-  }
+    Serial.print("[HTTP] PUT...\n");
+    // start connection and send HTTP header
+    //int httpCode = http.GET();
+    int httpCode0;
 
-  httpCode0 = http.PUT(payload0);
-  Serial.print("Putting: ");
-  Serial.println(payload0);
+    http.addHeader("content-type", "application/json", false, false);
+    //http.addHeader("authorization", "basic YWRtaW5pc3RyYXRvcjpwYXNzd29yZA==", false, false);
 
-  // httpCode will be negative on error
-  if (httpCode0 > 0) {
+    String payload0;
+
+    if (rearstatus)
+    {
+      //payload0 = "{\"state\": \"true\"}\r\n";
+      payload0 = "{\"heater\":{\"state\":{\"on1\":true}}}";
+    }
+    else
+    {
+      //payload0 = "{\"state\": \"false\"}\r\n";
+      payload0 = "{\"heater\":{\"state\":{\"on1\":false}}}";
+    }
+
+    httpCode0 = http.PUT(payload0);
+    Serial.print("Putting: ");
+    Serial.println(payload0);
+
+    // httpCode will be negative on error
+    if (httpCode0 > 0) {
     // HTTP header has been send and Server response header has been handled
     Serial.printf("[HTTP] GET... code: %d\n", httpCode0);
 
@@ -78,9 +96,25 @@ void updateServerState(int serverStateMode) //reportToBackendServer
       String payload = http.getString();
       Serial.println(payload);
     }
-  } else {
+    } else {
     Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode0).c_str());
+    }
+  */
+
+  // httpCode will be negative on error
+  if (httpCode > 0) {
+    // HTTP header has been send and Server response header has been handled
+    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+    // file found at server
+    if (httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println(payload);
+    }
+  } else {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
   }
+
 
   http.end();
 }
